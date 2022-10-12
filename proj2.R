6e51b1eecb87638974fe711a13136d2242b97ec6
Passara Chanchotisatien <31198485+passaraink@users.noreply.github.com>
2022-10-12 16:12:10 +0100

    create proj2 file and some comments

---
+++ proj2.R
@@ -0,0 +1,34 @@
+# Statistical Programming Coursework 2
+
+# Members and contributions
+
+
+# Code description
+
+
+
+# Question 1
+
+# This question contains code for the function pone, which takes in n, k, strategy,
+# and nreps as arguments and returns the probability of one prisoner succeeding in
+# finding their number. k (integer) is the prisoner number and strategy (integer)
+# indicates which strategy (an integer 1 to 3) to estimate the probability for.
+
+
+# Question 2
+
+
+
+# Question 3
+
+
+
+# Question 4
+
+
+
+# Question 5
+
+
+
+# Question 6
