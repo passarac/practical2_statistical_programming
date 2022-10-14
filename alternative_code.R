463f8de1b0f0771e11d7d55e49528e4ec7344f48
Passara Chanchotisatien <31198485+passaraink@users.noreply.github.com>
2022-10-14 22:32:51 +0100

    just adding a test file, can ignore

--- alternative_code.R
+++ alternative_code.R
@@ -7,21 +7,21 @@
 # CODE DESCRIPTION
 
 # The purpose of this code is to stimulate the prisoner problem which is defined as follows (from coursework spec):
-  # • 2n prisoners each have a unique prisoner number from 1 to 2n.
-  # • The prison contains a room in which there are 2n boxes, each with a unique number from 1 to 2n painted
-  # on its lid.
-  # • 2n cards, each printed with a unique number from 1 to 2n, are randomly placed one in each box.
-  # • The prisoners have the task of finding the card with their number on it by opening a maximum on n boxes.
-  # • After each prisoner’s go, the room is returned exactly to its original state and the prisoner is not allowed to
-  # communicate with prisoners yet to have their go.
-  # • If all prisoners succeed in finding their number, then they all go free.
+# • 2n prisoners each have a unique prisoner number from 1 to 2n.
+# • The prison contains a room in which there are 2n boxes, each with a unique number from 1 to 2n painted
+# on its lid.
+# • 2n cards, each printed with a unique number from 1 to 2n, are randomly placed one in each box.
+# • The prisoners have the task of finding the card with their number on it by opening a maximum on n boxes.
+# • After each prisoner’s go, the room is returned exactly to its original state and the prisoner is not allowed to
+# communicate with prisoners yet to have their go.
+# • If all prisoners succeed in finding their number, then they all go free.
 
 # This code calculates the probability of prisoners going free if they follow one of 3 possible strategies (from coursework spec):
-  # Strategy 1: The prisoner starts at the box with their number on it, opens it and reads the number on the card: k, say. If k
-  # is not their prisoner number, they go to box number k, open it and repeat the process until they have either
-  # found the card with their number on it, or opened n boxes without finding it.
-  # Strategy 2: As strategy 1, but starting from a randomly selected box.
-  # Strategy 3: They open n boxes at random, checking each card for their number.
+# Strategy 1: The prisoner starts at the box with their number on it, opens it and reads the number on the card: k, say. If k
+# is not their prisoner number, they go to box number k, open it and repeat the process until they have either
+# found the card with their number on it, or opened n boxes without finding it.
+# Strategy 2: As strategy 1, but starting from a randomly selected box.
+# Strategy 3: They open n boxes at random, checking each card for their number.
 
 
 # Helper / Additional functions
@@ -42,7 +42,7 @@ is_successful <- function(n, pris_n, card_num, strategy) {
 }
 
 
-# Question 1 -----------------------------------------------------------------------------------------------------------
+# Question 1 ---------------------------------------------------------------------------------------------------
 
 
 pone <- function(n, k, strategy, nreps) {
@@ -54,7 +54,8 @@ pone <- function(n, k, strategy, nreps) {
 }
 
 
-# Question 2 ------------------------------------------------------------------------------------------------------------
+
+# Question 2 ----------------------------------------------------------------------------------------------------
 
 pall <- function(n, strategy, nreps) {
   success_vec <- rep(0,nreps)
@@ -70,7 +71,7 @@ pall <- function(n, strategy, nreps) {
 }
 
 
-# Question 3 -------------------------------------------------------------------------------------------------------------
+# Question 3 ----------------------------------------------------------------------------------------------------
 
 # Here, we estimate the individual and joint success probabilities for each strategy
 # for when n = 5 and n = 50.
@@ -105,7 +106,7 @@ cat("Strategy 3 resulted in probability of ", unlist(pall(n,3,num_trials)[1]), "
 
 
 
-# Question 4 -----------------------------------------------------------------------------------------------------------
+# Question 4 -------------------------------------------------------------------------------------------------
 
 # In this section, we elaborate why the results are surprising.
 
